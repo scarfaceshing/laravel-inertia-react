e39da1fe1680b99b0c9179185b473e903474bb2f
scarfaceshing <edrianjansolatorio@gmail.com>
2023-10-18 23:36:59 +0000

    pull from feature/employees and updated docker file

---
+++ app/Listeners/ImportEmployeeListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Events\ImportEmployeesEvent;
+
+class ImportEmployeeListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(ImportEmployeesEvent $event)
+    {
+        //
+
+        return $event;
+    }
+}
